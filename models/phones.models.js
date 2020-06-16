bd46527ce27dfded7ae8f64d30e9eaf75f9932fa
Dablakbandit <ashle.thew@gmail.com>
2020-06-17 10:34:20 +1200

    Fixes

--- models/phones.models.js
+++ models/phones.models.js
@@ -20,6 +20,8 @@ Phones.list = (result) => {
 };
 
 Phones.modelSearch = (model, result) => {
+    var str = "";
+    var strNew = str.substr(0, str.lastIndexOf(" "));
     var query = sql.query("SELECT * FROM `phones` WHERE `model` LIKE " + sql.escape(model) + ";", (err, res) => {
         if (err) {
             console.log("error: ", err);
